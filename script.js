eb31d499a59b946ea79a21114420e18583262083
dkpdhoni-eng <dkpdhoni@gmail.com>
2026-05-25 01:32:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function joinNow() {
+
+    alert("Thank You For Joining Green Civic Mission!");
+
+}function joinNow() {
+
+    alert("Thank You For Joining Green Civic Mission!");
+
+}function joinNow() {
+
+    alert("Thank You For Joining Green Civic Mission!");
+
+}function joinNow(){
+
+    alert("Thank You For Joining Green Civic Mission!");
+
+}function darkMode(){
+
+    document.body.classList.toggle("dark");
+
+}
+
+
+
+/* VOLUNTEER COUNTER */
+
+let count = 0;
+
+let counter = document.getElementById("counter");
+
+let interval = setInterval(function(){
+
+    count++;
+
+    counter.innerHTML = count;
+
+    if(count == 02){
+
+        clearInterval(interval);
+
+    }
+
+}, 20);
